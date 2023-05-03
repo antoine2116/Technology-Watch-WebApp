d7cb16972c56fc0a638be20abe87eb3d09222d19
Ilias <ilias.kelesoglu@globule.net>
2023-05-03 23:16:49 +0200

    Search function

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Navbar from "./Navbar";
+import Search from "@/components/Filters/Search";
 
 function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -18,6 +19,7 @@ function Layout({ children }: { children: React.ReactNode }) {
 
       <div className="antialiased bg-white text-slate-900">
         <Navbar />
+        <Search/>
         <div className="flex w-full">
           <div className="flex-auto py-4 px-6">
             <div className="container mx-auto">
